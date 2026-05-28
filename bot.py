ad5f81ca2660c9196b168c5e19d22aa2f2310930
Niveditha <nivedithakatta57@gmail.com>
2026-05-28 19:37:21 +0530

    add roast command

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from dotenv import load_dotenv
 import os
 
@@ -20,5 +21,21 @@ async def on_message(message):
 
     if message.content == '!hello':
         await message.channel.send('Hello! I am alive!')
+
+    if message.content.startswith('!roast'):
+        roasts = [
+            "You're the reason we have warning labels on everything.",
+            "I'd roast you but my mom said I'm not allowed to burn trash.",
+            "You're not stupid, you just have bad luck thinking.",
+            "I'd explain it to you but I don't have crayons with me.",
+            "You're like a cloud. When you disappear, it's a beautiful day.",
+        ]
+        
+        if message.mentions:
+            target = message.mentions[0]
+            roast = random.choice(roasts)
+            await message.channel.send(f"{target.mention} {roast}")
+        else:
+            await message.channel.send("Who do you want me to roast? Tag someone! e.g. `!roast @someone`")
 
 client.run(os.getenv('DISCORD_TOKEN'))
