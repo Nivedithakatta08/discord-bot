f6a3d770ea382c629d1df3138b6ad1c2f0532c36
Niveditha <nivedithakatta57@gmail.com>
2026-06-07 22:32:59 +0530

    add avatar command

--- bot.py
+++ bot.py
@@ -160,11 +160,11 @@ async def on_message(message):
             )
 
     if message.content.startswith('!avatar'):
-     if message.mentions:
-        target = message.mentions[0]
-    else:
-        target = message.author
-    await message.channel.send(f"🖼️ **{target.display_name}'s Avatar**\n{target.display_avatar.url}")
+       if message.mentions:
+            target = message.mentions[0]
+       else:
+            target = message.author
+       await message.channel.send(f"🖼️ **{target.display_name}'s Avatar**\n{target.display_avatar.url}")
 
 
 client.run(os.getenv('DISCORD_TOKEN'))
